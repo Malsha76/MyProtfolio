a721d98f168b03b64abc94cee096ab1430b2bd65
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-05-25 11:28:54 +0530

    SPA

---
+++ Assignments/SPA/model/customerDTO.js
@@ -0,0 +1,8 @@
+class CustomerDTO {
+    constructor(customerID, customerName, customerAddress, customerSalary) {
+        this.id = customerID;
+        this.name = customerName;
+        this.address = customerAddress;
+        this.salary = customerSalary;
+    }
+}
